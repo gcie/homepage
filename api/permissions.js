e80d73a266cdc39ee75cdb226a1ef5c5e00b48fa
Grzegorz Ciesielski <tao24.k@gmail.com>
2020-02-11 13:27:00 +0100

    chore: update version in .lock, redirect on auth error

--- api/permissions.js
+++ api/permissions.js
@@ -3,21 +3,17 @@ module.exports = {
         return {
             authenticated: authenticated,
             admin: (req, res, next) => {
-                if (req.user.group === 'admin') {
+                if (req && req.user && req.user.group === 'admin') {
                     next();
                 } else {
-                    res.status(401).json({
-                        error: `Insufficient permissions. Access requires admin permissions, but you have ${req.user.group} permissions`
-                    });
+                    res.redirect('/');
                 }
             },
             manager: (req, res, next) => {
-                if (req.user.group === 'manager' || req.user.group === 'admin') {
+                if (req && req.user && (req.user.group === 'manager' || req.user.group === 'admin')) {
                     next();
                 } else {
-                    res.status(401).json({
-                        error: `Insufficient permissions. Access requires manager permissions, but you have ${req.user.group} permissions`
-                    });
+                    res.redirect('/');
                 }
             }
         };
